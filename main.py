89dbd4a1a2230c1bb1d337ce057b8b4cf58d28af
jephte <jephtetraore1@gmail.com>
2025-01-02 13:51:00 -0500

    first

---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, request
+import pymysql
+
+# Configuration de la base de données
+connection = pymysql.connect(
+    host="sql5.freesqldatabase.com",
+    user="sql5755343",
+    password="jt6JRIJgJB",
+    database="sql5755343",
+    port=3306,
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+app = Flask(__name__)
+
+# Route pour récupérer les informations des athlètes
+@app.route('/athletes', methods=['GET'])
+def get_all_player_info():
+    try:
+        with connection.cursor() as cursor:
+            sql = """
+                SELECT 
+                    a.firstName AS AthleteFirstName,
+                    a.lastName AS AthleteLastName,
+                    a.athletesId AS athletesId,
+                    a.teamId AS teamId,
+                    a.img AS AthleteImage,
+                    t.teamName AS TeamName,
+                    t.sportName AS SportName,
+                    t.division AS DivisionName
+                FROM 
+                    athletes a
+                LEFT JOIN 
+                    team t ON a.teamId = t.teamId;
+                """
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            return jsonify(results), 200  # Renvoie les résultats sous forme JSON
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Route pour vérifier les tables spécifiques pour un athlète donné
+@app.route('/athlete/<athlete_id>/stats', methods=['GET'])
+def my_home_screen(athlete_id):
+    tables = [
+        "StatQB", "StatRB", "StatWR",
+        "StatDef", "StatPunt", "StatPuntReturn",
+        "StatKickoff", "StatKickoffReturn"
+    ]
+    total = []
+
+    try:
+        with connection.cursor() as cursor:
+            for table in tables:
+                sql = f"SELECT athletesId FROM {table} WHERE athletesId = %s AND hasStats = 1"
+                cursor.execute(sql, (athlete_id,))
+                result = cursor.fetchone()
+
+                if result:
+                    sql = f"SELECT * FROM {table} WHERE athletesId = %s"
+                    cursor.execute(sql, (athlete_id,))
+                    exist = cursor.fetchone()
+                    total.append(exist)  # Ajout des statistiques
+            return jsonify(total), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Lancement de l'application Flask
+if __name__ == '__main__':
+    app.run(debug=True,)
