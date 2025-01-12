5651fdc36f9cae25048629c42bcb4a55b32202e3
jephte <jephtetraore1@gmail.com>
2025-01-11 20:05:00 -0500

    first

--- backend.py
+++ backend.py
@@ -6,14 +6,14 @@ from flask import g
 def get_db():
     if 'db' not in g:
         g.db = pymysql.connect(
-            host="sql5.freesqldatabase.com",
-            user="sql5755343",
-            password="jt6JRIJgJB",
-            database="sql5755343",
-            port=3306,
-            charset="utf8mb4",
-            cursorclass=pymysql.cursors.DictCursor
-        )
+            host="sql5.freesqldatabase.com",  # Serveur local
+            user="sql5755343",  # Utilisateur par défaut
+            password="jt6JRIJgJB",  # Mot de passe vide par défaut (remplacez si modifié)
+            database="sql5755343",  # Nom de votre base de données
+            port=3306,  # Port MySQL par défaut (modifiable dans XAMPP)
+            charset="utf8mb4",  # Encodage des caractères
+            cursorclass=pymysql.cursors.DictCursor  # Pour recevoir les résultats sous forme de dictionnaired
+)
     return g.db
 
 def close_db(error=None):
@@ -22,26 +22,75 @@ def close_db(error=None):
         db.close()
 
 def get_all_players():
-    db = get_db()
-    with db.cursor() as cursor:
-        sql = """
-            SELECT 
-                a.firstName AS AthleteFirstName,
-                a.lastName AS AthleteLastName,
-                a.athletesId AS athletesId,
-                a.teamId AS teamId,
-                a.img AS AthleteImage,
-                t.teamName AS TeamName,
-                t.sportName AS SportName,
-                t.division AS DivisionName
-            FROM 
-                athletes a
-            LEFT JOIN 
-                team t ON a.teamId = t.teamId;
-        """
-        cursor.execute(sql)
-        results = cursor.fetchall()
-    return results
+    try:
+        db = get_db()
+        with db.cursor() as cursor:
+            sql = """
+                SELECT 
+                    a.firstName AS AthleteFirstName,
+                    a.lastName AS AthleteLastName,
+                    a.athletesId AS athletesId,
+                    a.teamId AS teamId,
+                    a.img AS AthleteImage,
+                    t.teamName AS TeamName,
+                    t.sportName AS SportName,
+                    t.division AS DivisionName
+                FROM 
+                    athletes a
+                LEFT JOIN 
+                    team t ON a.teamId = t.teamId;
+            """
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            players = [
+                {
+                    "full_name": f"{row['AthleteFirstName']} {row['AthleteLastName']}",
+                    "athletesId": row['athletesId'],
+                    "teamId": row['teamId'],
+                    "AthleteImage": row['AthleteImage'],
+                    "TeamName": row['TeamName'],
+                    "sportName": row['SportName'],
+                    "division": row['DivisionName']
+                }
+                for row in results
+            ]
+        return players
+    except Exception as e:
+        print(f"Error fetching players: {e}")
+        return []
+    finally:
+        close_db()
+def get_all_teams():
+    try:
+        db = get_db()
+        with db.cursor() as cursor:
+            sql = """
+                SELECT 
+                    t.teamId AS teamId,
+                    t.teamName AS TeamName,
+                    t.sportName AS SportName,
+                    t.division AS DivisionName
+                FROM 
+                    team t
+            """
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            players = [
+                {
+                    "teamId": row['teamId'],
+                    "TeamName": row['TeamName'],
+                    "sportName": row['SportName'],
+                    "division": row['DivisionName']
+                }
+                for row in results
+            ]
+        return players
+    except Exception as e:
+        print(f"Error fetching players: {e}")
+        return []
+    finally:
+        close_db()
+
 
 def get_athlete_stats(athlete_id):
     tables = [
@@ -52,16 +101,114 @@ def get_athlete_stats(athlete_id):
     total = []
 
     db = get_db()
-    with db.cursor() as cursor:
+    with db.cursor() as cursor:  # Utiliser des dictionnaires pour un accès facile
         for table in tables:
+            # Vérifier si des statistiques existent pour l'athlète dans la table actuelle
             sql = f"SELECT athletesId FROM {table} WHERE athletesId = %s AND hasStats = 1"
             cursor.execute(sql, (athlete_id,))
             result = cursor.fetchone()
 
             if result:
+                # Récupérer toutes les colonnes pour l'athlète dans la table actuelle
                 sql = f"SELECT * FROM {table} WHERE athletesId = %s"
                 cursor.execute(sql, (athlete_id,))
-                exist = cursor.fetchone()
-                if exist:
-                    total.append(exist)
+                rows = cursor.fetchall()
+                if rows:
+                    for row in rows:
+                        for key, value in row.items():
+                            # Ignorer les colonnes qui ne sont pas des statistiques
+                            if key not in ["athletesId", "hasStats"]:
+                                total.append({
+                                    "stateName": key,
+                                    "stat": value
+                                })
+
     return total
+
+
+def get_game(team_id, order="DESC"):
+    db = get_db()
+    try:
+        with db.cursor() as cursor:
+            sql = f"""
+                SELECT 
+                    tg.gameId,
+                    tg.gameDate,
+                    tg.visitingTeamId,
+                    tg.homeTeamId,
+                    tg.visitingTeamScore,
+                    tg.homeTeamScore,
+                    t1.teamName AS visitingTeamName,
+                    t2.teamName AS homeTeamName,
+                    gs.*
+
+
+
+
+
+
+
+
+
+
+                FROM 
+                    TeamGames tg
+                LEFT JOIN 
+                    GameStat gs ON tg.gameId = gs.GameId
+                LEFT JOIN 
+                    team t1 ON tg.visitingTeamId = t1.teamId
+                LEFT JOIN 
+                    team t2 ON tg.homeTeamId = t2.teamId
+                WHERE 
+                    tg.visitingTeamId = %s OR tg.homeTeamId = %s
+                ORDER BY 
+                    tg.gameDate {order};
+                """
+            cursor.execute(sql, (team_id, team_id))
+            results = cursor.fetchall()
+            print(results)
+            return results
+
+    except Exception as e:
+        print(f"Erreur : {e}")
+        return None
+
+
+def get_athlete(athlete_id):
+    db = get_db()  # Connexion à la base de données
+    try:
+        with db.cursor() as cursor:
+            # Requête pour récupérer les informations de l'athlète
+            sql = """
+            SELECT 
+                a.athletesId,
+                a.firstName,
+                a.lastName,
+                a.position,
+                a.number,
+                a.height,
+                a.weight,
+                a.img,
+                t.teamId,
+                t.teamName,
+                t.sportName,
+                t.division,
+                t.primaryColor,
+                t.secondaryColor
+            FROM 
+                athletes a
+            LEFT JOIN 
+                team t ON a.teamId = t.teamId
+            WHERE 
+                a.athletesId = %s
+            """
+            cursor.execute(sql, (athlete_id,))
+            result = cursor.fetchone()
+
+            if not result:
+                return {"error": "Athlete not found"}
+            return result
+    except Exception as e:
+        print(f"Erreur : {e}")
+        return {"error": str(e)}
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # app.py
+from flask import Flask, jsonify,request
 
-from flask import Flask, jsonify
-from backend import get_all_players, get_athlete_stats, close_db
+from backend import get_all_players, get_athlete_stats, close_db, get_all_teams, get_game, get_athlete
 from flask import g
 
 app = Flask(__name__)
@@ -11,22 +11,71 @@ def teardown_db(exception):
     close_db(exception)
 
 # Route pour récupérer les informations des athlètes
-@app.route('/athletes', methods=['GET'])
+@app.route('/athletes', methods=['POST','GET'])
 def athletes():
     try:
-        players = get_all_players()
-        return jsonify(players), 200
+            players = get_all_players()
+            return jsonify(players), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Route pour récupérer les statistiques d'un athlète spécifique
-@app.route('/athlete/<athlete_id>/stats', methods=['GET'])
-def athlete_stats(athlete_id):
+@app.route('/team', methods=['POST','GET'])
+def teams():
     try:
+            players = get_all_teams()
+            return jsonify(players), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# Route pour récupérer les statistiques d'un athlète spécifique
+@app.route('/athlete/stats',methods=['POST'])
+def athlete_stats():
+    print(request.get_json())
+
+    data = request.get_json()
+
+    if not data or 'athlete_id' not in data:
+        return jsonify({"error": "Missing athlete_id"}), 400
+    athlete_id = data['athlete_id']
+    try:
+
         stats = get_athlete_stats(athlete_id)
         return jsonify(stats), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+@app.route('/game',methods=['POST'])
+def athlete_game():
+    print(request.get_json())
+
+    data = request.get_json()
+
+    if not data or 'team_id' not in data:
+        return jsonify({"error": "Missing athlete_id"}), 400
+    athlete_id = data['team_id']
+    try:
+
+        stats = get_game(athlete_id)
+        return jsonify(stats), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/athlete',methods=['POST'])
+def athlete():
+    print(request.get_json())
+
+    data = request.get_json()
+
+    if not data or 'athlete_id' not in data:
+        return jsonify({"error": "Missing athlete_id"}), 400
+    athlete_id = data['athlete_id']
+    try:
+
+        stats = get_athlete(athlete_id)
+        return jsonify(stats), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
